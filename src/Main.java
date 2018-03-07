a518ede9df57ac998125b9b0cd33fccb34a07d30
Dillion Driskell <dill.driskell@gmail.com>
2018-03-07 02:56:06 -0600

    Added code to read file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+	
+	Graph g;	//Graph the user will be working with
 	
 	public static void main(String[] args){
 		
@@ -18,23 +21,28 @@ public class Main {
 		
 		//Code for getting the user's choice, with minor input validation
 		System.out.print("User's choice: ");
+		/* TODO : UNBLOCK, UNIT TESTING
 		while(userChoice < 1 || userChoice > 4){
 			userChoice = in.nextInt();
 			if(userChoice < 1 || userChoice > 4)
 				System.out.print("\nPlease make another selection: ");
 		}
-		
+		*/
+		userChoice = 1;
 		//Execute the user's choice
 		switch(userChoice){
 		case 1:
 			System.out.println("You have chosen to perform BFS.");
+			adjacencyListToGraph();
 			break;
 		case 2:
 			System.out.println("You have chosen to perform DFS.");
+			adjacencyListToGraph();
 			break;
 		case 3:
 			System.out.println("Please type the address of the file you would like to use.");
 			System.out.println(/*TODO CLEANUP*/ "e.g. C://BLAH/DIRECTORY");
+			//TODO : adjacencyListToGraph(<<file location >>)
 			break;
 		case 4:
 			System.out.println("Thank you for using the program.");
@@ -44,5 +52,36 @@ public class Main {
 		
 		
 	}
-
+	
+	
+	/**
+	 * Takes the default file res/DFSBFSinput.dat
+	 * as the input adjacency list matrix, ands
+	 * turns it into a graph
+	 * 
+	 * @return void
+	 */
+	
+	public static void adjacencyListToGraph(){
+		String line = null;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("res/DFSBFSinput.dat"));
+			
+			while((line = br.readLine()) != null){
+				addVertexToGraph(line);
+			}
+			
+			br.close();
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	// TODO override adjacencyListToGraph() with arguments that accepts arguments of file location
+	
+	public static void addVertexToGraph(String v){
+		System.out.println(v);
+	}
 }
